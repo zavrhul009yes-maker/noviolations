fc706de5ff2b47dae254d07d25c5d4fc3ceb9624
zavrhul009yes-maker <zavrhul.009.yes@gmail.com>
2026-04-01 12:37:13 +0500

    Создание миграций

---
+++ database/migrations/2026_04_01_071937_create_reposts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reposts', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->text ('description');
+            $table->timestamps();
+            $table->foreignId('user_id')
+            ->nullable()
+            ->constrained()
+            ->cascadeOnDelete()
+            ->nullOnDelete();
+            $table->foreignId('status_id')
+            ->nullable()
+            ->constrained()
+            ->cascadeOnDelete()
+            ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reposts');
+    }
+};
